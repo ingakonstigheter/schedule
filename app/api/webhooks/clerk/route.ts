1f42178ac32b6d5c5a8ee61b012b0c5ee9a949e9
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-10-24 11:48:09 +0200

    no verification on webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -5,41 +5,8 @@ import { createUser, deleteUser } from "../../../../lib/data/user";
 import { User } from "../../../../lib/types/types";
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-  if (!WEBHOOK_SECRET) {
-    throw new Error("Add CLER_WEBHOOK_SECRET from Clerk dashboard");
-  }
-  const headerPayload = headers();
-  const svix_id = (await headerPayload).get("svix-id");
-  const svix_timestamp = (await headerPayload).get("svix-timestamp");
-  const svix_signature = (await headerPayload).get("svix-signature");
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
-
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
-  /* 
-  const wh = new Webhook(WEBHOOK_SECRET);
-  let evt: WebhookEvent;
-  try {
-    evt = wh.verify(body, {
-      svix_id: svix_id,
-      svix_timestamp: svix_timestamp,
-      svix_signature: svix_signature,
-    }) as WebhookEvent;
-  } catch (error) {
-    console.error("Error verifying webhook:", error);
-    return new Response("Error occured", {
-      status: 400,
-    });
-  }
-
-  const eventType = evt.type;
- */
-  const { type, data } = payload;
+  const body = await req.json();
+  const { type, data } = body;
   if (type === "user.created") {
     const { id, email_addresses, first_name, last_name } = data;
     const user = {
@@ -51,26 +18,35 @@ export async function POST(req: Request) {
     };
     try {
       const response = await createUser(user as User);
-      console.log(response.success);
-      /*   if (!response.success) {
+      if (!response.success) {
         return new Response("Error occurd when syncing with database", {
           status: 400,
         });
-      } */
+      }
+      return new Response("OK", { status: 200 });
     } catch (err) {
       console.log("error creating user", err);
-    }
-    return new Response("OK", { status: 200 });
-  }
-  if (type === "user.deleted") {
-    const response = await deleteUser(data.id);
-    if (!response.success) {
       return new Response("Error occurd when syncing with database", {
         status: 400,
       });
     }
-    return new Response("OK", { status: 200 });
+  }
+  if (type === "user.deleted") {
+    try {
+      const response = await deleteUser(data.id);
+      if (!response.success) {
+        return new Response("Error occurd when syncing with database", {
+          status: 400,
+        });
+      }
+      return new Response("OK", { status: 200 });
+    } catch (err) {
+      console.log("error creating user", err);
+      return new Response("Error occurd when syncing with database", {
+        status: 400,
+      });
+    }
   } else {
-    return new Response("OK", { status: 200 });
+    return new Response("ERROR", { status: 400 });
   }
 }
